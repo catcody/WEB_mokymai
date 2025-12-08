e89d891e2a8932398ee1df079cda211ee3cc8e71
stonytem <stonyte.m@gmail.com>
2025-12-08 13:29:00 +0200

    update kas antras kintamasis su operacija " x+= 2;"

--- Marija task 12.1/script.js
+++ Marija task 12.1/script.js
@@ -8,15 +8,14 @@ for(let i = 0; i <= 10; i++){
     console.log(i);
 }
 
+
 // 2. Parašyti for, kuris išvestų kas antrą skaičių pradedant 0 ir baigiant 15.
 console.log("\n- - - - 2. kas antras skaicius nuo 0 iki 15");
 
-for(let x = 0; x <= 15; x++){
-    if(x % 2 == 0){
-        continue;
-    }
+for(let x = 0; x <= 15; x+= 2){
     console.log(x);
 }
+
 
 // 3. Parašyti for, kuris išvestų kas trečią skaičių, pradedant 1 ir baigiant 20. Kiekvieną skaičių apskliausti laužtiniais skliaustais. Pvz.: [1][4][7]...
 console.log("\n- - - - 3. kas trecias skaicius [1-20]");
@@ -35,6 +34,7 @@ for(let sk = 1; sk<= 20; sk++){
     }
 };
 
+
 // 5. Susikurkite kintamuosius rėžių pradžiai ir pabaigai nusakyti. Patikrinkite, kad tai būtų validu (pradžia turi būti mažesnė nei pabaiga). Jei rėžiai tinkami, tuomet vykdyti for, kuris atskirose eilutėse išvestų kiekvieną skaičių iš tų rėžių, bei atskiriant tarpu - tų skaičių kvadratus.
 console.log("\n- - - - 5. skaiciu seka ir kvadratai");
 
@@ -44,6 +44,7 @@ let end = 8;
 for(start = 1; start < end; start++){
     console.log(`${start} ${start**2}`);
 }
+
 
 // 6. Susikurkite kintamuosius rėžių pradžiai ir pabaigai nusakyti. Patikrinkite, kad tai būtų validu (pradžia turi būti mažesnė nei pabaiga). Jei rėžiai tinkami, tuomet vykdyti for, kuris iš duotų skaičių išvestų visus nelyginius skaičius arba tuos, kurie dalinasi iš 8.
 console.log("\n- - - - 6. nelyginiai skaiciai arba kurie dalinasi is 8");
@@ -60,14 +61,6 @@ for(pradzia = 1; pradzia <= pabaiga; pradzia++){
 
 // 7. (papildomai, sunkiau) Be daugybos veiksmo programoje, sudauginti du skaičius.
 console.log("\n- - - - 7. be daugybos veiksmo programoje, sudauginti du skaicius");
-
-// let ats = 0;
-// let daugiklis = 8;
-
-// for(let kin = 2; kin <= daugiklis; kin++){
-//     ats += daugiklis;
-// }
-// console.log(ats);
 
 function multiply(skaicius, daugiklis) {
   let ats = 0;
